9058777d46d6a093ae0e5797528df65725a0c7f1
calebcodesgud <calwoy1@gmail.com>
2022-11-27 18:57:05 -0500

    cleaned up code

--- barlow/main.py
+++ barlow/main.py
@@ -197,7 +197,6 @@ class BarlowTwins(nn.Module):
 
         # sum the cross-correlation matrix between all gpus
         c.div_(self.args.batch_size)
-        # torch.distributed.all_reduce(c)
 
         on_diag = torch.diagonal(c).add_(-1).pow_(2).sum()
         off_diag = off_diagonal(c).pow_(2).sum()
