b1d7955989722b3935d73c3857681e3f5bd85350
Shawn W. Abernethy Jr <shawn.abernethy@gmail.com>
2022-12-05 18:05:02 -0500

    Added a notebook for loading models and showing images.

---
+++ datasets/dataset.py
@@ -0,0 +1,54 @@
+import argparse
+import torch
+import os
+import pickle
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset
+from typing import List, Tuple
+
+from img_transform.transforms import EyeMaskCustomTransform, EyeDatasetCustomTransform
+
+
+IMG_TRANSFORMS = transforms.Compose([
+    transforms.ToTensor(),
+    EyeDatasetCustomTransform(mask_threshold=0.25),
+])
+
+
+LBL_TRANSFORMS = transforms.Compose([
+    transforms.ToTensor(),
+    EyeMaskCustomTransform(mask_threshold=0.25),
+])
+
+
+class RetinaSegmentationDataset(Dataset):
+    def __init__(self, rootdir: str,
+                 basenames: List,
+                 img_transforms: torch.nn.Module = IMG_TRANSFORMS,
+                 lbl_transforms: torch.nn.Module = LBL_TRANSFORMS):
+        self._rootdir = rootdir
+        self._basenames = basenames
+        self._img_transforms = img_transforms
+        self._lbl_transforms = lbl_transforms
+        
+    def __len__(self) -> int:
+        return len(self._basenames)
+    
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        img_path = os.path.join(self._rootdir, "images", self._basenames[index])
+        lbl_path = os.path.join(self._rootdir, "labels", self._basenames[index])
+        
+        with open(img_path, "rb") as f:
+            img = pickle.load(f)
+            # Apply the transforms for the image
+            img = self._img_transforms(img)
+            
+        #'''
+        with open(lbl_path, "rb") as f:
+            lbl = pickle.load(f)
+            # Apply the transforms for the labels
+            lbl = self._lbl_transforms(lbl)
+        #'''
+            
+        return img, lbl
+        
--- main.py
+++ main.py
@@ -1,60 +1,12 @@
 import argparse
 import torch
 import os
-import pickle
-import torchvision.transforms as transforms
-from torch.utils.data import Dataset
-from typing import List
 from pathlib import Path
 from tqdm import tqdm
 
-from img_transform.transforms import EyeMaskCustomTransform, EyeDatasetCustomTransform
 from model.unet import Unet, DEFAULT_UNET_LAYERS
 from model.dice_loss import DiceLoss, DiceBCELoss
-
-
-IMG_TRANSFORMS = transforms.Compose([
-    transforms.ToTensor(),
-    EyeDatasetCustomTransform(mask_threshold=0.25),
-])
-
-
-LBL_TRANSFORMS = transforms.Compose([
-    transforms.ToTensor(),
-    EyeMaskCustomTransform(mask_threshold=0.25),
-])
-
-
-class RetinaSegmentationDataset(Dataset):
-    def __init__(self, rootdir: str,
-                 basenames: List,
-                 img_transforms: torch.nn.Module = IMG_TRANSFORMS,
-                 lbl_transforms: torch.nn.Module = LBL_TRANSFORMS):
-        self._rootdir = rootdir
-        self._basenames = basenames
-        self._img_transforms = img_transforms
-        self._lbl_transforms = lbl_transforms
-        
-    def __len__(self) -> int:
-        return len(self._basenames)
-    
-    def __getitem__(self, index: int) -> (torch.Tensor, torch.Tensor):
-        img_path = os.path.join(self._rootdir, "images", self._basenames[index])
-        lbl_path = os.path.join(self._rootdir, "labels", self._basenames[index])
-        
-        with open(img_path, "rb") as f:
-            img = pickle.load(f)
-            # Apply the transforms for the image
-            img = self._img_transforms(img)
-            
-        #'''
-        with open(lbl_path, "rb") as f:
-            lbl = pickle.load(f)
-            # Apply the transforms for the labels
-            lbl = self._lbl_transforms(lbl)
-        #'''
-            
-        return img, lbl
+from datasets.dataset import RetinaSegmentationDataset
 
 
 def train_model(model, dataloader, criterion, optimizer, device):
